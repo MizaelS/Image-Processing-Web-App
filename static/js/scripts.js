1de25ffa93312b84f4bc50596c8889a2ece6b5f8
MizaelS <sansoresangel97@gmail.com>
2023-08-26 10:11:39 -0600

    Update

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,24 +1,31 @@
 function submitForm(formId) {
     var form = document.getElementById(formId);
-    var formData = new FormData(form);
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', form.action, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            var blob = new Blob([xhr.response], { type: 'application/octet-stream' });
-            var link = document.createElement('a');
-            link.href = window.URL.createObjectURL(blob);
-            var format = formId === 'resizeForm' ? 'resizeFormat' : 'convertFormat';
-            link.download = 'output.' + document.getElementById(format).value.toLowerCase();
-            link.click();
-            document.getElementById('loading').style.display = 'none';
-        }
-    };
-    xhr.responseType = 'arraybuffer';
-    xhr.send(formData);
-    document.getElementById('loading').style.display = 'block';
+    if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+    } else {
+        var formData = new FormData(form);
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', form.action, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                var blob = new Blob([xhr.response], { type: 'application/octet-stream' });
+                var link = document.createElement('a');
+                link.href = window.URL.createObjectURL(blob);
+                var format = formId === 'resizeForm' ? 'resizeFormat' : 'convertFormat';
+                link.download = 'output.' + document.getElementById(format).value.toLowerCase();
+                link.click();
+                document.getElementById('loading').style.display = 'none';
+            }
+        };
+        xhr.responseType = 'arraybuffer';
+        xhr.send(formData);
+        document.getElementById('loading').style.display = 'block';
+    }
+    form.classList.add('was-validated');
     return false;
 }
+
 
 document.getElementById('resizeButton').addEventListener('click', function () {
     document.getElementById('resizeDiv').style.display = 'block';
