6f9cb2a1bac2a71cce3b1f559096f842a0b827ca
MizaelS <sansoresangel97@gmail.com>
2023-08-26 14:04:03 -0600

    UI redesign, Added image background removal, Added discord webhook

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template, request, send_file
 from PIL import Image
-from flask import send_file
 Image.MAX_IMAGE_PIXELS = None
+from rembg import remove
 import os
+import io
+from discord_webhook import DiscordWebhook
 
 app = Flask(__name__)
 
+webhook_url = ""
+
 @app.route('/')
 def index():
+    client_ip = request.remote_addr
+    webhook = DiscordWebhook(url=webhook_url, content=f'Un usuario ha abierto la página. IP: {client_ip}')
+    webhook.execute()
     return render_template('index.html')
-
 
 @app.route('/convert', methods=['POST'])
 def convert_image():
@@ -24,6 +30,8 @@ def convert_image():
         
         # Enviar archivo y luego eliminarlo
         try:
+            webhook = DiscordWebhook(url=webhook_url, content='Un usuario ha convertido una imagen')
+            webhook.execute()
             return send_file(filename, as_attachment=True)
         finally:
             os.remove(filename)
@@ -46,13 +54,41 @@ def resize_image():
         
         # Enviar archivo y luego eliminarlo
         try:
+            webhook = DiscordWebhook(url=webhook_url, content='Un usuario ha redimensionado una imagen')
+            webhook.execute()
             return send_file(filename, as_attachment=True)
         finally:
             os.remove(filename)
     else:
         return 'Error: Todos los campos son requeridos.'
-
+    
+@app.route('/remove_background', methods=['POST'])
+def remove_background():
+    file = request.files['file']
+    
+    if file:
+        print("Recibido archivo")
+        img_bytes = file.read()
+        print("Leídos bytes del archivo")
+        output_bytes = remove(img_bytes)
+        print("Fondo removido")
+        output_img = Image.open(io.BytesIO(output_bytes))
+        filename = 'background_removed.png'
+        output_img.save(filename)
+        print("Imagen guardada")
+        
+        # Enviar archivo y luego eliminarlo
+        try:
+            print("Enviando archivo")
+            webhook = DiscordWebhook(url=webhook_url, content='Un usuario ha removido el fondo de una imagen')
+            webhook.execute()
+            return send_file(filename, as_attachment=True)
+        finally:
+            print("Eliminando archivo")
+            os.remove(filename)
+    else:
+        print("Error: Todos los campos son requeridos.")
+        return 'Error: Todos los campos son requeridos.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -12,20 +12,28 @@ function submitForm(formId) {
         xhr.upload.onprogress = function(event) {
             if (event.lengthComputable) {
                 var percentage = (event.loaded / event.total) * 100;
-                document.getElementById('progress').innerText = '' + percentage.toFixed(2) + '%';
+                document.getElementById('progress').innerText = 'Cargando imagen: ' + percentage.toFixed(2) + '%';
+                if (percentage === 100) {
+                    document.getElementById('progress').innerText = 'Procesando imagen...';
+                }
             }
         };
 
         xhr.onreadystatechange = function() {
             if (xhr.readyState == 4 && xhr.status == 200) {
-                var blob = new Blob([xhr.response], { type: 'application/octet-stream' });
+                var blob;
+                if (formId === 'removeBackgroundForm') {
+                    blob = new Blob([xhr.response], { type: 'image/png' });
+                } else {
+                    blob = new Blob([xhr.response], { type: 'application/octet-stream' });
+                }
                 var link = document.createElement('a');
                 link.href = window.URL.createObjectURL(blob);
-                var fileInput = formId === 'resizeForm' ? document.getElementById('file') : document.getElementById('convertFile');
+                var fileInput = formId === 'resizeForm' ? document.getElementById('file') : (formId === 'convertForm' ? document.getElementById('convertFile') : document.getElementById('removeBackgroundFile'));
                 var fileName = fileInput.files[0].name;
                 var fileExtension = fileName.split('.').pop();
-                var format = formId === 'resizeForm' ? 'resizeFormat' : 'convertFormat';
-                var selectedFormat = document.getElementById(format).value.toLowerCase();
+                var format = formId === 'resizeForm' ? 'resizeFormat' : (formId === 'convertForm' ? 'convertFormat' : 'png');
+                var selectedFormat = document.getElementById(format) ? document.getElementById(format).value.toLowerCase() : 'png';
                 link.download = fileName.replace(fileExtension, selectedFormat);
                 link.click();
                 document.getElementById('loading').style.display = 'none';
@@ -41,16 +49,42 @@ function submitForm(formId) {
 }
 
 
+
+
+
 document.getElementById('resizeButton').addEventListener('click', function () {
     document.getElementById('resizeDiv').style.display = 'block';
     document.getElementById('convertDiv').style.display = 'none';
+    document.getElementById('removeBackgroundDiv').style.display = 'none';
     document.getElementById('resizeButton').classList.add('active');
     document.getElementById('convertButton').classList.remove('active');
+    document.getElementById('removeBackgroundButton').classList.remove('active');
 });
 
 document.getElementById('convertButton').addEventListener('click', function () {
     document.getElementById('convertDiv').style.display = 'block';
     document.getElementById('resizeDiv').style.display = 'none';
+    document.getElementById('removeBackgroundDiv').style.display = 'none';
     document.getElementById('convertButton').classList.add('active');
     document.getElementById('resizeButton').classList.remove('active');
+    document.getElementById('removeBackgroundButton').classList.remove('active');
 });
+
+document.getElementById('removeBackgroundButton').addEventListener('click', function () {
+    document.getElementById('removeBackgroundDiv').style.display = 'block';
+    document.getElementById('resizeDiv').style.display = 'none';
+    document.getElementById('convertDiv').style.display = 'none';
+    document.getElementById('removeBackgroundButton').classList.add('active');
+    document.getElementById('resizeButton').classList.remove('active');
+    document.getElementById('convertButton').classList.remove('active');
+});
+
+document.getElementById('teramont-logo').addEventListener('mouseover', function () {
+    this.style.filter = 'grayscale(0%)';
+});
+
+document.getElementById('teramont-logo').addEventListener('mouseout', function () {
+    this.style.filter = 'grayscale(100%)';
+});
+
+
